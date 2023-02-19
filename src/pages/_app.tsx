1fa775d7d33b0dd2f82a3db86dbab89bb4e80c6f
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-02-19 15:34:36 +0900

    test: kakao auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,16 @@ import HeadLayout from '../components/Layout/Head';
 import { extendedTheme } from '../chakraTheme';
 import '../style/main.scss';
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
-        <ChakraBaseProvider theme={extendedTheme}>
-            <HeadLayout title={'Retromall에 오신 것을 환영합니다.'} />
-            <Component {...pageProps} />
-        </ChakraBaseProvider>
+        <SessionProvider>
+            <ChakraBaseProvider theme={extendedTheme}>
+                <HeadLayout title={'Retromall에 오신 것을 환영합니다.'} />
+                <Component {...pageProps} />
+            </ChakraBaseProvider>
+        </SessionProvider>
     );
 };
 export default App;
