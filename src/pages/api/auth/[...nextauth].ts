4d86e469a503cb5110d32c4a80a97102184bb774
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-02-23 15:13:21 +0900

    feat: auth-provider secret 속성 추가

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,5 +7,6 @@ export default NextAuth({
             clientId: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID!,
             clientSecret: process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET!
         })
-    ]
+    ],
+    secret: process.env.NEXT_PUBLIC_SECRET
 });
