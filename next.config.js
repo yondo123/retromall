feddddda6d82cfa9f92cc1b9af25d40bcf582047
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-03-09 21:13:44 +0900

    chore: Product Request URL

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 
 module.exports = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')]
-    },
-    reactStrictMode: true,
-    async rewrites() {
-        return [
-            {
-                source: '/req/:path*',
-                destination: `${process.env.NEXT_PUBLIC_API_REQUEST_URL}/api/:path*`
-            }
-        ];
-    }
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  },
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/req/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_REQUEST_URL_PRODUCT}/api/:path*`
+      }
+    ];
+  }
 };
