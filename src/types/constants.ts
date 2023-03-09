f1ef21814bfc2825c9489a44382e6a6e553f77d2
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-03-09 21:04:13 +0900

    feat: constants 타입 정의

---
+++ src/types/constants.ts
@@ -0,0 +1,16 @@
+type Constants = {
+  colors: Record<string, `#${string}`>;
+};
+
+export const constants: Constants = {
+  colors: {
+    font: '#2f4858',
+    border: '#dfe0df',
+    primary: '#f78018',
+    secondary: '#c55089',
+    tertiary: '#825795',
+    accent: '#f2585e',
+    white: '#fff',
+    black: '#000'
+  }
+};
