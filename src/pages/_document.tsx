e14be9be84fdf02a51a2ad11efe2c65b6adddd47
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-03-09 20:51:13 +0900

    add: Next Document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
