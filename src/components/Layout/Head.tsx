c2943bfbef28d34edfd324b76cc0ba536eb7f9de
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-03-09 20:46:23 +0900

    feat: viewport 수정

--- src/components/Layout/Head.tsx
+++ src/components/Layout/Head.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import Head from 'next/head';
 
 interface HeadProps {
-    title: string;
+  title: string;
 }
 const HeadLayout = (props: HeadProps) => {
-    const { title } = props;
+  const { title } = props;
 
-    return (
-        <Head>
-            <meta charSet="utf-8" />
-            <title>{title}</title>
-        </Head>
-    );
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" httpEquiv="X-UA-Compatible" />
+      <meta charSet="utf-8" />
+      <title>{title}</title>
+    </Head>
+  );
 };
 
 export default HeadLayout;
