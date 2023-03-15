917389ca1fdfa3eac1dcec6a95be2c5bc61696dd
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-03-15 23:51:53 +0900

    feat: 물품 등록 요청 API

--- src/api/product.ts
+++ src/api/product.ts
@@ -1,8 +1,25 @@
 import request from './instance';
+import { Product } from '../types/product';
 
 export const getCategories = () => {
-    return request({
-        url: 'req/categories',
-        method: 'get'
-    });
+  return request({
+    url: '/req/categories',
+    method: 'get'
+  });
+};
+
+export const registerProduct = (product: Product) => {
+  const { content, amount, images, category, hashTags, thumbnail } = product;
+  return request({
+    url: '/req/products',
+    method: 'post',
+    data: {
+      content,
+      amount,
+      category,
+      thumbnail,
+      images,
+      hashTags
+    }
+  });
 };
