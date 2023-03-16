9a166756ba850bad8132b75d6dbbfbe61df23e84
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-03-16 19:27:50 +0900

    test: 카카오로그인 테스트

--- src/pages/auth/kakao.tsx
+++ src/pages/auth/kakao.tsx
@@ -1,17 +1,27 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { kakaoLogin } from '../../api/auth';
 
 const Kakao = () => {
-    const router = useRouter();
-    const { code } = router.query;
-    useEffect(() => {
-        return () => {
-            router.push('/');
-            console.log(code);
-        };
-    }, [code]);
+  const router = useRouter();
+  const { code } = router.query;
+  useEffect(() => {
+    return () => {
+      console.log('kakao', code);
+      router.push('/');
+      if (code) {
+        kakaoLogin(code)
+          .then((response) => {
+            console.log(JSON.stringify(response));
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      }
+    };
+  }, [code]);
 
-    return <></>;
+  return <h2>Loading...</h2>;
 };
 
 export default Kakao;
