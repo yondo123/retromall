befdd874e5d7b9f241016082bd1e19bd925aa21c
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-03-16 19:21:48 +0900

    test: 카카오로그인 테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import Navbar from '../components/Layout/Navbar';
 import ProductContainer from '../components/Card/ProductContainer';
 import ProductItem from '../components/Card/ProductItem';
+import Link from 'next/link';
 const Home = () => {
   return (
     <div>
+      <button>
+        <Link
+          href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_DEV}`}
+        >
+          <a>카카오테스트</a>
+        </Link>
+      </button>
       <Navbar />
       <ProductContainer>
         <ProductItem />
