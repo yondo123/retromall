f0ed9e0d4c6e27e3b691d5eb2461d61f8c48e48c
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-03-23 23:43:56 +0900

    feat: Mobile Header 컴포넌트 구현

---
+++ src/components/Layout/MobileHeader.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import style from './styles/header.module.scss';
+import ChevronLeft from '../Icons/ChevronLeft';
+import Button from './Button';
+import { constants } from '../../types/constants';
+import { useRouterHandler } from '../../utils/useRouterHandler';
+
+interface HeaderProps {
+  title: string;
+}
+
+const MobileHaeder = (props: HeaderProps) => {
+  const { handleBackButton } = useRouterHandler();
+  const { colors } = constants;
+
+  return (
+    <header className={style['header-wrapper']}>
+      <h1 className={'visually-hidden'}>Header</h1>
+      <div className={style.header}>
+        <Button variants={'ghost'} size={'md'} onClick={handleBackButton}>
+          <ChevronLeft width={32} height={32} color={colors.font} />
+        </Button>
+        <h2 className={style['header-title']}>{props.title}</h2>
+      </div>
+    </header>
+  );
+};
+
+export default MobileHaeder;
