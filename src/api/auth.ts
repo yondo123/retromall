a19d69ed25296eb468f5828607496dc9aed954aa
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-03-23 23:47:30 +0900

    style: 주석 제거

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,6 @@
 import request from './instance';
 
 export const kakaoLogin = (authorizationCode?: string | string[]) => {
-  console.log('또잉', authorizationCode);
   return request({
     url: '/req/members/login',
     method: 'post',
