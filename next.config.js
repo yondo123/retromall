7c0dbf1ebf3ea61fb2b8bb9156a41fa1389d732c
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-03-23 23:34:10 +0900

    chore: reactStrictMode false

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
   },
-  reactStrictMode: true,
+  reactStrictMode: false,
   async rewrites() {
     return [
       {
