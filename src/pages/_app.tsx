64ed4eca9a36b53a3cb3ea0dc06a371c2077c7c7
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-03-23 23:32:44 +0900

    style: 의미없는 주석 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HeadLayout from '../components/Layout/Head';
 import { SessionProvider } from 'next-auth/react';
 import '../styles/reset.scss';
 import '../styles/main.scss';
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: any) => {
+  useEffect(() => {
+    console.log('하이로');
+  }, []);
   return (
     <SessionProvider session={session}>
       <HeadLayout title={'Retromall에 오신 것을 환영합니다.'} />
