905a9495cb0cc16782a00926e39ca4440c80fc94
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-03-25 22:04:00 +0900

    feat: ProductItem, ProductDetailItem 타입 정의

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,4 +1,3 @@
-export type Hashtags = string[];
 export type ImageUrls = string[];
 
 export interface Product {
@@ -7,5 +6,24 @@ export interface Product {
   category: string;
   thumbnail: string;
   images: ImageUrls;
-  hashTags: Hashtags;
+  hashTags: string[];
+}
+
+export interface ProductItem {
+  productId: number;
+  author: string;
+  content: string;
+  amount: number;
+  likes: number;
+  thumbnail: string;
+  createdAt: string;
+  modifiedAt: string;
+}
+
+export interface ProductDetailItem extends Omit<ProductItem, 'author' | 'thumbnail'> {
+  category: string;
+  isLiked: boolean;
+  hashTags: string[];
+  images: string[];
+  isAuthor: string;
 }
