b5536b32e40ce945d41e81e5588edb4bac6ea658
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-03-30 18:39:47 +0900

    feat: input onInput EventHandler 추가

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import style from './styles/input.module.scss';
 
 interface InputProps {
-    variants?: 'full' | 'center';
-    placeholder: string;
+  variants?: 'full' | 'center';
+  placeholder: string;
+  type?: 'text' | 'number' | 'tel';
+  id?: string;
+  onInput?: React.KeyboardEventHandler<HTMLInputElement>;
 }
 const Input = (props: InputProps) => {
-    return (
-        <div className={style.center}>
-            <input type="text" placeholder={props.placeholder} className={style.search} />
-        </div>
-    );
+  const { type = 'text', placeholder, variants = 'full', onInput } = props;
+  return (
+    <div className={style.center}>
+      <input type={type} placeholder={placeholder} className={style.search} onInput={onInput} />
+    </div>
+  );
 };
 export default Input;
